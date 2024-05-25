172de9a0cadbd8932abd4ec1bedbb03b4df12b0c
peterson <petersonsimiao1@gmail.com>
2024-05-25 14:10:06 -0300

    feat: Criado decorator que converte o decodifica o token jwt

--- src/shared/decorators/user.decorator.ts
+++ src/shared/decorators/user.decorator.ts
@@ -1,7 +1,12 @@
 import { ExecutionContext, createParamDecorator } from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
 
 export const User = createParamDecorator((field, context: ExecutionContext) => {
     const request = context.switchToHttp().getRequest();
     if (field && request[field]) return request[field]
-    return request.token;
+
+    const jwt = new JwtService();
+    const token = jwt.decode(request.headers.authorization.split(" ")[1]);
+
+    return token;
 })

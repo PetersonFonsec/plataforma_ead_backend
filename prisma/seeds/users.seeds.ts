26bcbf194c3c5d62b4ca6363fec5e33504e7bfef
peterson <petersonsimiao1@gmail.com>
2025-08-21 00:04:56 -0300

    test

--- prisma/seeds/users.seeds.ts
+++ prisma/seeds/users.seeds.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from '@prisma/client';
 
-import { user_director, user_student, user_teacher } from "../../test/__mock__/users";
+import { user_director, user_student, user_teacher, user_director_secundary } from "../../test/__mock__/users";
 
 const prisma = new PrismaClient()
 
@@ -9,6 +9,12 @@ export async function createUsers() {
     where: { email: user_director.email },
     update: {},
     create: user_director
+  });
+
+  const director_secundary = await prisma.user.upsert({
+    where: { email: user_director_secundary.email },
+    update: {},
+    create: user_director_secundary
   });
 
   const student = await prisma.user.upsert({
@@ -24,6 +30,6 @@ export async function createUsers() {
   });
 
   console.log('====== Usuarios Criados ======');
-  console.log({ director, student, teacher });
-  return { director, student, teacher };
+  console.log({ director, student, teacher, director_secundary });
+  return { director, student, teacher, director_secundary };
 }
--- test/__mock__/course.ts
+++ test/__mock__/course.ts
@@ -1,4 +1,5 @@
 export const course = {
   name: 'Conceitos de computação I',
+  description: 'Curso introdutório aos conceitos básicos de computação, abordando temas como hardware, software, sistemas operacionais e redes. Ideal para iniciantes que desejam entender os fundamentos da computação. Inclui aulas teóricas e práticas para uma compreensão completa.',
   thumb: '2bc5647a-6236-4cc6-57e0-d2b6a267fb00',
 }
